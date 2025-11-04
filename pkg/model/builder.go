a4c7bdd4ca99d168eaf570d2bc00577f62690895
goliatone <140582+goliatone@users.noreply.github.com>
2025-11-04 14:16:27 -0800

    add: support UI schema

--- pkg/model/builder.go
+++ pkg/model/builder.go
@@ -1,20 +1,25 @@
 package model
 
 import (
-	"github.com/goliatone/formgen/internal/model"
+	"errors"
+
+	internalmodel "github.com/goliatone/formgen/internal/model"
 	pkgopenapi "github.com/goliatone/formgen/pkg/openapi"
 )
 
-// Builder converts OpenAPI operations into form models.
+// Builder converts OpenAPI operations into form models and applies optional
+// decorators for UI schema overlays.
 type Builder interface {
 	Build(op pkgopenapi.Operation) (FormModel, error)
+	Decorate(form *FormModel) error
 }
 
 // BuilderOption configures the builder behaviour.
 type BuilderOption func(*builderOptions)
 
 type builderOptions struct {
-	labeler func(string) string
+	labeler    func(string) string
+	decorators []Decorator
 }
 
 // WithLabeler overrides the default label generation function.
@@ -24,17 +29,63 @@ func WithLabeler(labeler func(string) string) BuilderOption {
 	}
 }
 
+// WithDecorators registers decorators that should run when Decorate is called.
+func WithDecorators(decorators ...Decorator) BuilderOption {
+	return func(opts *builderOptions) {
+		if len(decorators) == 0 {
+			return
+		}
+		opts.decorators = append(opts.decorators, decorators...)
+	}
+}
+
 // NewBuilder returns a Builder backed by the internal implementation.
 func NewBuilder(options ...BuilderOption) Builder {
 	cfg := builderOptions{}
 	for _, opt := range options {
+		if opt == nil {
+			continue
+		}
 		opt(&cfg)
 	}
 
-	internalOpts := model.Options{}
+	internalOpts := internalmodel.Options{}
 	if cfg.labeler != nil {
 		internalOpts.Labeler = cfg.labeler
 	}
 
-	return model.New(internalOpts)
+	return &builder{
+		delegate:   internalmodel.New(internalOpts),
+		decorators: append([]Decorator(nil), cfg.decorators...),
+	}
+}
+
+type builder struct {
+	delegate   *internalmodel.Builder
+	decorators []Decorator
+}
+
+func (b *builder) Build(op pkgopenapi.Operation) (FormModel, error) {
+	if b == nil || b.delegate == nil {
+		return FormModel{}, errors.New("model: builder delegate is nil")
+	}
+	return b.delegate.Build(op)
+}
+
+func (b *builder) Decorate(form *FormModel) error {
+	if form == nil {
+		return errors.New("model: form is nil")
+	}
+	if b == nil {
+		return nil
+	}
+	for _, decorator := range b.decorators {
+		if decorator == nil {
+			continue
+		}
+		if err := decorator.Decorate(form); err != nil {
+			return err
+		}
+	}
+	return nil
 }
