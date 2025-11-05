cace2a9b70fbafb429535fb6d0efeaba940717d7
goliatone <140582+goliatone@users.noreply.github.com>
2025-11-05 09:00:09 -0800

    fix: configure client dev workflow

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: [
     "./dev/**/*.{html,js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./tests/**/*.{js,ts,jsx,tsx}",
+    "./data/**/*.{json,html}",
+    "../examples/http/**/*.{go,html,tmpl}",
+    "../pkg/renderers/**/*.{tmpl,html}",
+    "../pkg/renderers/vanilla/testdata/**/*.{html,json}",
+    "../pkg/uischema/**/*.{json,yml,yaml}",
+    "./node_modules/preline/preline.js",
+  ],
+  safelist: [
+    "min-w-[6rem]",
+    "ring-offset-white",
+    "ring-blue-500",
+    "shadow-xl",
+    "max-h-48",
+    "max-h-56",
   ],
   theme: {
     extend: {},
