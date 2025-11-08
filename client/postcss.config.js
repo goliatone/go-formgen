e99a8c8824e88c6f7647846a2bf89c35a99740f1
goliatone <140582+goliatone@users.noreply.github.com>
2025-11-07 19:06:09 -0800

    add: formgen JS behaviors

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,15 @@
+import postcssImport from "postcss-import";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    postcssImport(),
+    tailwindcss(),
+    autoprefixer(),
+    ...(isProduction ? [cssnano({ preset: "default" })] : []),
+  ],
+};
