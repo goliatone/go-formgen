237f0100bdbaa368ae3cca8f971cdbbea1b814cd
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-27 18:45:22 -0800

    add: model extensions

---
+++ internal/model/extensions.go
@@ -0,0 +1,9 @@
+package model
+
+// ParseUIExtensions extracts metadata and UI hints from x-formgen/x-admin
+// extensions. It returns nil maps when no supported metadata is found.
+func ParseUIExtensions(ext map[string]any) (map[string]string, map[string]string) {
+	metadata := metadataFromExtensions(ext)
+	uiHints := filterUIHints(metadata)
+	return metadata, uiHints
+}
