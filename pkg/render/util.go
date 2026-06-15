f6fb3ff8f8eee2feb927a95b9491bebedb1448f8
goliatone <140582+goliatone@users.noreply.github.com>
2026-06-14 18:06:57 -0700

    add: redaction helper

---
+++ pkg/render/util.go
@@ -0,0 +1,16 @@
+package render
+
+import "fmt"
+
+func anyToString(value any) string {
+	switch v := value.(type) {
+	case string:
+		return v
+	case fmt.Stringer:
+		return v.String()
+	case nil:
+		return ""
+	default:
+		return fmt.Sprint(v)
+	}
+}
